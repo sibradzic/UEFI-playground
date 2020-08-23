2104b201df3a5e502e6e16f07f65bfc73c347ca4
sibradzic <5964548+sibradzic@users.noreply.github.com>
2020-08-23 16:13:06 +0900

    Fix comment typos

--- fix_vendor_hashes.py
+++ fix_vendor_hashes.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 #
-# This sript is supposed to be able to check and fix "Vendor Hash File" found
+# This script is supposed to be able to check and fix "Vendor Hash File" found
 # on some recent Lenovo ThinkPad UEFI ROMs. Vendor Hash File is special UEFI
 # data module consisting a table of sha256 hashes of some of the FFSv2 UEFI
 # volumes found within the same UEFI ROM image.
 #
-# Ealry in the UEFI boot process the actual sha256 hashes of FFSv2 volumes are
+# Early in the UEFI boot process the actual sha256 hashes of FFSv2 volumes are
 # being compared with values in the Vendor Hash File, and in case some manual
 # changes in actual volumes the hashes will mismatch, resulting in boot failure
 # (on a particular ThinkPad that I've seen this problem, turning the machine on
-# results in black screen and a weird 'error code' melody being played)
+# results in black screen and a weird 'error code' melody being played).
+#
+# This script uses UEFITools binaries to do the heavy lifting of extracting and
+# FFSv2 and replacing Vendor Hash File modules. It will download UEFIExtract
+# and UEFIReplace binaries unless they are already in the working directory.
 #
 # For latest version check https://github.com/sibradzic/UEFI-playground
 #
